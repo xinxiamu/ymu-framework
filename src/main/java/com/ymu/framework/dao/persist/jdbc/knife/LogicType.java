8a1770416269886a3f8dbd2a8d83f0d0c06d2c8c
xinxiamu <932852117@qq.com>
2017-07-31 11:09:50 +0800

    来自第三方对spring jdb的封装库。

---
+++ src/main/java/com/ymu/framework/dao/persist/jdbc/knife/LogicType.java
@@ -0,0 +1,5 @@
+package com.ymu.framework.dao.persist.jdbc.knife;
+
+public enum LogicType {
+	AND, OR;
+}
---
+++ src/main/java/com/ymu/framework/dao/persist/jdbc/knife/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 对spring jdbc的一个封装使用。来自第三方。
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.ymu.framework.dao.persist.jdbc.knife;
