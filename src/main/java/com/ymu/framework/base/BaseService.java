f34fe5280ed981aed8137ee20d0c9490ca4ed0e2
xinxiamu <932852117@qq.com>
2018-08-31 09:21:32 +0800

    修复

--- src/main/java/com/ymu/framework/base/BaseService.java
+++ src/main/java/com/ymu/framework/base/BaseService.java
@@ -1,7 +1,9 @@
 package com.ymu.framework.base;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 public class BaseService {
 
     @Value("${app.tmp-dir}")
